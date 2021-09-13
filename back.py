172dc1e07ed2cc86e19cf3b36248f507443a081e
harsh-del <64789547+harsh-del@users.noreply.github.com>
2021-09-14 00:23:13 +0530

    Update back.py

--- back.py
+++ back.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
 
-import subprocess
-
-print("content-type:  text/html")
-print("Access-Control-Allow-origin: *")
-print()
-
-
-o = subprocess.getoutput("date")
-print(o)
-[root@localhost cgi-bin]# cat back.py 
-#!/usr/bin/python3
-
 import cgi
 import subprocess
 import time
